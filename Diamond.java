c2263d4c82d54ac4e237cbb0912c57a6a225781b
Sami-ur-Rehman <samiurrehman2395@gmail.com>
2024-07-21 23:17:37 +0500

    Add files via upload

---
+++ Diamond.java
@@ -0,0 +1,34 @@
+public class Diamond {
+    public static void main(String[] args) {
+        int n = 5; // You can change this value to make a larger or smaller diamond
+
+        // Upper part of the diamond
+        for (int i = 1; i <= n; i++) {
+            // Print leading spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            // Move to the next line
+            System.out.println();
+        }
+
+        // Lower part of the diamond
+        for (int i = n - 1; i >= 1; i--) {
+            // Print leading spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            // Move to the next line
+            System.out.println();
+        }
+    }
+}
+
