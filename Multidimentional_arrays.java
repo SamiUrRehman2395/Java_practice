5d10f88d4671dec03a515a0fb556cf51fad6809c
Sami-ur-Rehman <samiurrehman2395@gmail.com>
2024-07-30 14:01:34 +0500

    Add files via upload

---
+++ Multidimentional_arrays.java
@@ -0,0 +1,29 @@
+public class Multidimentional_arrays {
+    public static void main(String[] args) {
+
+        int [] marks; // 1-D array
+
+        int [][] flats; // 2-D array
+
+        flats  = new int [2][3];
+        // 3 flats on every floor
+        //and there are 2 floors
+        //ground floor and first floor
+
+        flats[0][0] = 101;
+        flats[0][1] = 102;
+        flats[0][2] = 103;
+        flats[1][0] = 201;
+        flats[1][1] = 202;
+        flats[1][2] = 203;
+
+        for (int i=0; i < flats.length; i++) {
+            for (int j = 0; j < flats[i].length; j++) {
+                System.out.print(flats[i][j]);
+                System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
+
+    }
+}
